310a2862e22a0e0fc205c1f06daffc5d4fccd96c
Brent J. Nordquist <brent@nordist.net>
2018-12-31 17:52:58 -0600

    Only reveal colors to spymaster

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -6,4 +6,12 @@ module GamesHelper
   def nav_new_aclass
     (@game && @game.new_record?) ? 'active' : ''
   end
+
+  def protected_who(game_word, session)
+    if game_word.game.spymaster?(session)
+      game_word.who
+    else
+      'nil'
+    end
+  end
 end
