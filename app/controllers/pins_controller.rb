601751966762b1e15c88a4fb99df7e5ac5a239f6
Brent J. Nordquist <brent@nordist.net>
2020-12-26 07:10:16 -0600

    PIN entry form

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,9 +3,38 @@ class PinsController < ApplicationController
   respond_to :html, :js
 
   def index
-    @game = Game.order(created_at: :desc).joins(:spymasters).where('spymasters.sessionid = ?', session.id).first
+    req = Rails.application.routes.recognize_path(request.referrer)
+    if req[:controller] == 'games' && req[:id].present?
+      @game = Game.find_by(id: req[:id].to_i)
+    end
   end
 
   def create
+    game_pin = params[:game_pin].gsub(/\D/, '')
+    if @game = Game.find_by(pin: game_pin)
+      @spymaster = @game.spymasters.build(sessionid: session.id)
+      if @spymaster.save
+        respond_to do |format|
+          format.html { redirect_to @game }
+          format.js { render js: "window.location='#{game_url(@game).to_s}';" }
+        end
+      else
+        Rails.logger.error "spymaster save failed: #{@spymaster.errors.inspect}"
+        # FIXME this alert is getting lost
+        flash.alert = 'Unable to update database with new spymaster'
+        respond_to do |format|
+          format.html { redirect_to root_url }
+          format.js { render js: "window.location='#{root_url.to_s}';" }
+        end
+      end
+    else
+      Rails.logger.error "game not found for PIN: #{game_pin}"
+      # FIXME this alert is getting lost
+      flash.alert = 'Invalid PIN'
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.js { render js: "window.location='#{root_url.to_s}';" }
+      end
+    end
   end
 end
