686116632028c7d339efe2dc766e9329e3fbafa2
Brent J. Nordquist <brent@nordist.net>
2020-12-26 11:34:03 -0600

    Accessibility and color improvements

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,10 +1,11 @@
 module GamesHelper
   def protected_who_classes(game_word, session)
-    if game_word.game.spymaster?(session) || game_word.revealed?
+    spymaster = game_word.game.spymaster?(session)
+    if spymaster || game_word.revealed?
       klass = game_word.who
     else
       klass = 'nil'
     end
-    game_word.revealed? ? klass : "#{klass} unrevealed"
+    game_word.revealed? ? (spymaster ? "#{klass} revealed" : klass) : "#{klass} unrevealed"
   end
 end
